ef45efa6893778f0543e80d60f7b55dc24e18ff3
lucas03170811 <lucas03170811@gmail.com>
2025-07-17 09:10:32 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
-def log_message(msg):
-    from datetime import datetime
-    now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{now}] {msg}")
+mport time
+from datetime import datetime
 
-def stop_loss_triggered(entry_price, current_price, max_loss=0.2):
-    return (current_price - entry_price) / entry_price <= -max_loss
+def get_timestamp():
+    return int(time.time() * 1000)
+
+def current_time():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
