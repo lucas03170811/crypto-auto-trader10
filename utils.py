6f3594e87ad4fed6e9e1a15a4772ada42ee4636d
lucas03170811 <lucas03170811@gmail.com>
2025-07-17 09:56:30 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
-mport time
+import time
 from datetime import datetime
-
-def get_timestamp():
-    return int(time.time() * 1000)
 
 def current_time():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def log_message(message):
+    timestamp = current_time()
+    print(f"[{timestamp}] {message}")
